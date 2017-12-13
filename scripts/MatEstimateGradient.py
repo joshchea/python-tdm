71271214084e352e6f115f7db655c843c1472a2e
Chetan Joshi <joshchea@vt.edu>
2017-12-13 07:48:11 -0800

    Matrix Estimation with Gradient Method
    Script for OD-matrix estimation with least squares formulation, solution method is via Gradient descent applied with sparse matrices. Distance measure - difference in counts and assigned model flows and OD cells from seed and current adjusted matrix.  
    Requires - numpy and scipy | flow matrix obtained from Visum -> user should construct their own if not using other tool for assignment.

---
+++ scripts/MatEstimateGradient.py
@@ -0,0 +1,111 @@
+import numpy as np
+import scipy.sparse
+import VisumPy.helpers as VPH
+import csv
+import time
+#Least squares matrix estimation with gradient method - Chetan Joshi, Klaus Noekel 
+
+#------------User input -----------------------------------------------------------------#
+matno = 41            # seed mat and also where result is stored
+countAttrID = "AddVal1" 
+wtAttrID = "AddVal2"
+wtOD = 42
+flowmatfile = r"C:\Projects\KA_Work\LSODME\FlowMatrix.mtx"
+#----------------------------------------------------------------------------------------#
+
+#Matrix estimation function: takes flow prportion matrix, OD seed, Count and returns adjusted matrix: Chetan Joshi, updated by Klaus Noekel
+def EstimateMatrix(FlowProp, FlowPropT, OD_Flows, Ca, Wt, iter=25):
+    #Va = numpy.sum(OD_Flows[:,numpy.newaxis] * FlowProp, axis=0)
+    #Wt[Wt < 1] = 1
+    Va = FlowPropT.dot(OD_Flows)
+    Visum.WriteToTrace('Length of Va' + str(len(Va)))
+    Visum.WriteToTrace('Length of Ca' + str(len(Ca)))
+    Z = sum((Va-Ca)**2)
+    Visum.WriteToTrace('Starting Z =' + str(Z))
+    #print 'Starting Z =', Z
+
+    for i in range(1, iter):
+        t1 = time.time()
+#        Grad = numpy.sum(FlowProp*(Va - Ca), axis=1) # sum over a in A
+        Grad = FlowProp.dot((Va - Ca)*Wt) 
+#        Va_prime = numpy.sum( -OD_Flows[:,numpy.newaxis] * Grad[:,numpy.newaxis] * FlowProp, axis=0)
+        Va_prime = FlowPropT.dot(-OD_Flows * Grad)
+        lambda_opt = sum((Ca - Va)*Va_prime)/sum(Va_prime*Va_prime) 
+        if Grad.max()> 0:
+            lambda_opt = min(lambda_opt, 1/Grad.max())
+        OD_Flows = OD_Flows*(1 - lambda_opt*Grad) #variant - 1.1
+        OD_Flows[OD_Flows<0]=0 #remove very small -0.0 values from matrix if any...
+        Va = FlowPropT.dot(OD_Flows)
+        Z = sum((Va-Ca)**2)
+        t2 = time.time()
+        if  Z < 1:
+            break;
+        else:
+            Visum.WriteToTrace(str(i) + ': Z =' + str(Z), True)
+            #Visum.WriteToTrace( 'Z =', Z, ' time = ', t2-t1
+
+    Visum.WriteToTrace('Final Z =' + str(Z), True)
+    #print 'Final Z =', Z
+    return OD_Flows
+
+def readFlowMat(nODs, nLinks, filename):
+    t1 = time.time()
+    with open(filename, "rb") as f:
+        reader = csv.reader(f, delimiter='\t')
+        for i in xrange(0, 12):
+            reader.next()
+        i = []
+        j = []
+        data = []
+        for row in reader: #each row = CountIndex \t ODIndex \t proportion
+            if row[0] == "": continue
+            if row[0].startswith("*"): continue
+            if row[0].startswith("0"): continue
+            i.append(long(row[1])-1)
+            j.append(long(row[0])-1)
+            data.append(float(row[2]))        
+    
+    ix = j[-1] + 1
+    z = max(i)
+    for k in xrange(len(Sparse_OD)):
+        # j-> link
+        i.append(long(k)) #index of od pair
+        j.append(long(ix))#index of link
+        data.append(1.0)
+        ix+=1
+        
+    FlowProp = scipy.sparse.csr_matrix((data, (i,j)), shape=(len(Sparse_OD), nLinks+nODs), dtype='d')
+    #Visum.WriteToTrace([[k, z, i[-1],j[-1]], [len(i), len(j)]])
+    t2 = time.time()
+    Visum.WriteToTrace("read flow mat: " + str(t2-t1) , True)
+
+    return FlowProp
+
+#--------------------------------------------------------------------------------------------------------
+Ca = np.array(VPH.GetMulti(Visum.Net.Links, countAttrID, True))
+Wt = np.array(VPH.GetMulti(Visum.Net.Links, wtAttrID, True))
+AssignedOD = VPH.GetMatrixRaw(Visum, matno).flatten() #Get the flattened seed matrix
+WeightOD = VPH.GetMatrixRaw(Visum, wtOD).flatten()   #Get the flattened weight matrix
+SparseWeightOD = WeightOD.compress(AssignedOD > 0).copy()
+Sparse_OD = AssignedOD.compress(AssignedOD > 0).copy() #Get only cells > 0 to reduce array size
+nLinks = Visum.Net.Links.CountActive # Get the number of active links based on filter - will be extended to turns if turns are used
+#OD_constraint_block = numpy.identity(len(Sparse_OD)) 
+Ca = np.append(Ca, Sparse_OD) #Extend the count array  to include delta with the existing OD matrix on the least squares formulation             
+#Wt = np.append(Wt, np.ones(len(Sparse_OD))) #Extend the weight array for weight matrix using default of 1.0, should be changed to
+Wt = np.append(Wt, SparseWeightOD) #Extend the weight array for weight matrix using default of 1.0, weight matrix values should be varied for testing 
+
+FlowProp = readFlowMat(len(Sparse_OD), nLinks, flowmatfile)
+FlowPropT = scipy.sparse.csc_matrix(FlowProp.T)
+
+Visum.WriteToTrace(FlowPropT.shape)
+Visum.WriteToTrace(FlowProp.shape)
+
+NewODFlows = EstimateMatrix(FlowProp, FlowPropT, Sparse_OD, Ca, Wt, iter=25)
+
+#Set back result to Visum...
+AssignedOD[AssignedOD>0] = NewODFlows
+nZones = Visum.Net.Zones.Count
+VPH.SetMatrixRaw(Visum, matno, AssignedOD.reshape((nZones,nZones)))
+Visum.WriteToTrace("results stored" , True)
+del FlowProp
+del FlowPropT
