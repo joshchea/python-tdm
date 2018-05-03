c44a03877192f65362be145bf734abf522d798a0
Chetan Joshi <joshchea@vt.edu>
2018-05-03 11:38:30 -0700

    Gravity model calibration function

--- scripts/CalibrateGravity.py
+++ scripts/CalibrateGravity.py
@@ -1,51 +1,50 @@
-#this script replicates the balmprod.mac macro in EMME2, multiple productions are balanced against one attraction with their 
-#own friction factor matrices. [Chetan Joshi, Portland OR October 2010]
+#-------------------------------------------------------------------------------
+# Name:        Calibrate gravity model 
+# Purpose:     Calibrates the coefficient for the gravity model with -ve exp friction factor
+#              given an average trip length, skim and P/A vectors
+# Author:      Chetan Joshi, Portland OR
+#
+# Created:     5/6/2015
+#              
+# Copyright:   (c) Chetan Joshi 2015
+# Licence:     Permission is hereby granted, free of charge, to any person obtaining a copy
+#              of this software and associated documentation files (the "Software"), to deal
+#              in the Software without restriction, including without limitation the rights
+#              to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#              copies of the Software, and to permit persons to whom the Software is
+#              furnished to do so, subject to the following conditions:
+#
+#              The above copyright notice and this permission notice shall be included in all
+#              copies or substantial portions of the Software.
+#
+#              THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#              IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#              FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#              LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#              SOFTWARE.
+#-------------------------------------------------------------------------------
 import numpy
-import VisumPy.helpers as h
 
-Skim = h.GetMatrixRaw(Visum,3) #Specify 1st friction matrix
-Prod1 = numpy.array(Visum.Net.Zones.GetMultiAttValues("Production(DestinationChoiceEst)"))[:,1]  #First production demand strata/vector
-Attr =  numpy.array(Visum.Net.Zones.GetMultiAttValues("Attraction(DestinationChoiceEst)"))[:,1]   #Attraction demand strata/vector 
-Trips1 = numpy.zeros((len(Prod1),len(Prod1)))
-
-ProdA = Prod1
-AttrA = Attr
-alpha = 2
-adjC = -0.01
-TargetAvgTripLen = 10.5
-
-fricMat1 = numpy.exp(adjC*Skim)
-for Iter in range(0,50):
+def CalibrateSinglyConstrained(P, A, avg_trip_len, skim, c = -0.1, alpha = 2, max_iter = 10):
+    ProdA = P
+    AttrA = A
+    num_zones = len(P)
+    trips = numpy.zeros((num_zones, num_zones))
+    fric_mat = numpy.exp(c*skim)
     
-    for i in range(0,len(Prod1)):
-        Trips1[i,:] = ProdA[i]*AttrA*fricMat1[i,:]/max(0.000001, sum(AttrA * fricMat1[i, :]))
+    for Iter in range(0, max_iter):
+        for i in range(0, num_zones):
+            trips[i,:] = ProdA[i]*AttrA*fric_mat[i,:]/max(0.000001, sum(AttrA * fric_mat[i,:]))
 
-    ComputedProductions = Trips1.sum(1)
-    ComputedProductions[ComputedProductions==0]=1
-    OrigFac = (Prod1/ComputedProductions)
-    ProdA = OrigFac*ProdA
+        model_trip_len = (trips*skim).sum() / trips.sum()
+        c = c*(model_trip_len / avg_trip_len)**alpha
+        fric_mat = numpy.exp(c*skim)
 
-    ComputedAttractions = Trips1.sum(0) 
-    ComputedAttractions[ComputedAttractions==0]=1
-    DestFac = (Attr/ComputedAttractions)
-    AttrA = DestFac*AttrA
-
-    ComputedTripLen = (Trips1*Skim).sum()/ Trips1.sum()
-    adjC = adjC*(ComputedTripLen / TargetAvgTripLen)**alpha
-    fricMat1 = numpy.exp(adjC*Skim)
-    if abs(ComputedTripLen - TargetAvgTripLen) <= 0.01:
-        break;
-    else:
-        Visum.WriteToTrace('Coefficient: '+str(adjC))
-        Visum.WriteToTrace('Average Trip Length: '+str(ComputedTripLen))
-
-Visum.WriteToTrace('Average Trip Length: '+str(ComputedTripLen))
-Visum.WriteToTrace('Coefficient: '+str(adjC))
-h.SetMatrixRaw(Visum,2011,Trips1)   #Key numbers of matrices where results will be stored in VISUM
-
-
-
-
-            
+        print ('iteration: ', Iter, ' coefficient: ', c, ' average trip length (model): ', model_trip_len)
         
-    
+    print ('target average trip length (observed): ', avg_trip_len) 
+    print ('final average trip length (model): ', model_trip_len)
+    print ('final logit scaling factor: ', c)
+    return trips   #returns final trip matrix corresponding to calibrated coefficient
