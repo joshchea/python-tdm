dc1ac4eae5067d792187da6e0e066174c807fc0b
Chetan Joshi <joshchea@vt.edu>
2021-04-13 07:51:11 -0700

    Update CalibrateGravity.py
    cleaned up a couple of things to use numpy instead of built-ins

--- scripts/CalibrateGravity.py
+++ scripts/CalibrateGravity.py
@@ -1,11 +1,11 @@
 #-------------------------------------------------------------------------------
-# Name:        Calibrate gravity model 
+# Name:        Calibrate gravity model
 # Purpose:     Calibrates the coefficient for the gravity model with -ve exp friction factor
 #              given an average trip length, skim and P/A vectors
 # Author:      Chetan Joshi, Portland OR
 #
 # Created:     5/6/2015
-#              
+#
 # Copyright:   (c) Chetan Joshi 2015
 # Licence:     Permission is hereby granted, free of charge, to any person obtaining a copy
 #              of this software and associated documentation files (the "Software"), to deal
@@ -25,26 +25,26 @@
 #              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #              SOFTWARE.
 #-------------------------------------------------------------------------------
-import numpy
+import numpy as np
 
 def CalibrateSinglyConstrained(P, A, avg_trip_len, skim, c = -0.1, alpha = 2, max_iter = 10):
     ProdA = P
     AttrA = A
     num_zones = len(P)
-    trips = numpy.zeros((num_zones, num_zones))
-    fric_mat = numpy.exp(c*skim)
-    
+    trips = np.zeros((num_zones, num_zones))
+    fric_mat = np.exp(c*skim)
+
     for Iter in range(0, max_iter):
         for i in range(0, num_zones):
-            trips[i,:] = ProdA[i]*AttrA*fric_mat[i,:]/max(0.000001, sum(AttrA * fric_mat[i,:]))
+            trips[i,:] = ProdA[i]*AttrA*fric_mat[i,:]/np.maximum(0.000001, np.sum(AttrA * fric_mat[i,:]))
 
         model_trip_len = (trips*skim).sum() / trips.sum()
         c = c*(model_trip_len / avg_trip_len)**alpha
-        fric_mat = numpy.exp(c*skim)
+        fric_mat = np.exp(c*skim)
 
         print ('iteration: ', Iter, ' coefficient: ', c, ' average trip length (model): ', model_trip_len)
-        
-    print ('target average trip length (observed): ', avg_trip_len) 
+
+    print ('target average trip length (observed): ', avg_trip_len)
     print ('final average trip length (model): ', model_trip_len)
     print ('final logit scaling factor: ', c)
     return trips   #returns final trip matrix corresponding to calibrated coefficient
